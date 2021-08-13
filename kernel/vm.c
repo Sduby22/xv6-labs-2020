f7c3c435c4830adec84be6394461903529bd801f
sduby <supersduby@gmail.com>
2021-08-13 09:35:25 +0800

    make the array static.

--- kernel/vm.c
+++ kernel/vm.c
@@ -17,7 +17,8 @@ extern char etext[];  // kernel.ld sets this to end of kernel code.
 
 extern char trampoline[]; // trampoline.S
 
-int ref[(PHYSTOP - KERNBASE) / PGSIZE] = {};
+static int ref[(PHYSTOP - KERNBASE) / PGSIZE] = {};
+
 #define Refindex(x) ((x-KERNBASE)/PGSIZE)
 /*
  * create a direct-map page table for the kernel.
